f771b29b1aca207fda15bf900a539eb882e49643
Pavel Lapushkin <zerocoolik@mail.ru>
2021-02-07 16:31:18 +0300

    Init Commit

---
+++ src/main/java/su/oilmap/OilMap/OilMapApplication.java
@@ -0,0 +1,22 @@
+package su.oilmap.OilMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class OilMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OilMapApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
